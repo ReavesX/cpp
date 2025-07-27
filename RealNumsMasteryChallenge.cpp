638330f502bc6785179e830638889485a6f57b7c
ReavesX <donjacks0n@proton.me>
2025-07-26 20:53:18 -0400

    Real Nums Mastery Challenge Challenge
    Easy
    Write a C++ program that:
    
    Declares a float variable named radius and initializes it with the value 4.5f
    Declares a double variable named pi and initializes it with the value 3.1415

---
+++ RealNumsMasteryChallenge.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+/*
+
+
+Challenge
+
+Easy
+Write a C++ program that:
+
+Declares a float variable named radius and initializes it with the value 4.5f
+Declares a double variable named pi and initializes it with the value 3.1415
+
+
+
+*/
+
+int main() {
+        
+    float radius = 4.5f;
+    double pi = 3.1415d;
+
+    // Don't change below this line
+    std::cout << "radius: " << radius << std::endl;
+    std::cout << "pi: " << pi << std::endl;
+    
+    return 0;
+}
